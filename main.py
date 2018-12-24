ffdc6bdcf23130de91a8886d761f6613e5bc35aa
GAG566 <thomas.legoff@engie.com>
2018-12-24 14:21:38 +0100

    add graph display

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from wind_forecast_provider import Wind_Forecast_Provider
 
 windfinder = Wind_Forecast_Provider("WindFinder")
 
-
 print(windfinder.get_provider_description())
 
-windfinder.display_windforecast("Zeebrugges")
+windfinder_forecast = windfinder.get_windforecast("")
+#windfinder.display_windforecast(windfinder_forecast)
+
+windfinder.display_windforecast_graph(windfinder_forecast)
--- wind_forecast_provider.py
+++ wind_forecast_provider.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from matplotlib import pyplot as nplt
+from matplotlib import pyplot as plt
+from random import *
 
 
 
@@ -29,17 +30,37 @@ class Wind_Forecast_Provider():
 
         for i in range(0,24):
             start_date = datetime(current_date.year,current_date.month,current_date.day+1, i)
-            self.windforecast[start_date] = [20+i , 25+i]
+            x = randint(-10, 10)
+            y = randint(-10,10)
+            self.windforecast[start_date] = [12+x , 14+y]
         return self.windforecast
 
-    def display_windforecast(self, spot):
-        windforecast = self.get_windforecast(spot)
+    def display_windforecast(self, windforecast):
         for key, value in windforecast.items():
             print("date: " + key.strftime("%d/%m/%Y - %H"))
             print(' '*4 + "average wind speed: " + str(value[0]) + " knots")
             print(' '*4 + "gust wind speed: " + str(value[1]) + " knots")
 
+    def get_forecast_date_list(self, windforecast):
+        date_list = []
+        for key in windforecast.keys():
+            date_list.append(key.strftime("%d/%m/%y - %H:00"))
+        return date_list
 
-    def display_windforecast(self,spot):
-        #display forecazst on a graph
+
+    def get_forecast_average_wind_speed_list(self, windforecast):
+        av_windspeed =[]
+        for value in windforecast.values():
+                av_windspeed.append(value[0])
+        return av_windspeed
+
+    def display_windforecast_graph(self, windforecast):
+        #display forecast on a graph
+        fig = plt.figure()
+        x = self.get_forecast_date_list(windforecast)
+        y=  self.get_forecast_average_wind_speed_list(windforecast)
+        plt.plot(x , y)
+        plt.ylabel('windspeed')
+        fig.autofmt_xdate()
+        plt.show()
 
